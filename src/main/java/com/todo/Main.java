262bde58bd91518f2ab927f8f08428d30ead4f48
lakshanaaaaa <lakshanasampath916@gmail.com>
2025-09-16 16:02:35 +0530

    DataBase Connection Established

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,6 +1,16 @@
-package com.todo;
+package main.java.com.todo;
+import java.sql.SQLException;
+import com.todo.util.DatabaseConnection;
+import java.sql.Connection;
 public class Main{
     public static void main(String args[]){
-
+        DatabaseConnection db_Connection = new DatabaseConnection();
+        try{
+            Connection cn= db_Connection.getDBConnection();
+            System.out.println("Connected to the database");
+        }
+        catch(SQLException e){
+            System.out.println("Failed to connect to the database" + e.getMessage());
+        }
     }
 }
